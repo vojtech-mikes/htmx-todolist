b056c6e7dc37ccee1eb3dc8d9ced1e789de1d01e
Vojtěch Mikeš <vojtechmikes98@gmail.com>
2023-12-23 00:26:55 +0100

    added some libs and dev script

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const PORT = 3000;
+
+const app = express();
+
+app.listen(PORT, () => {
+	console.log("Server is litening on port ", PORT);
+});
